ba74c60a97320e83781321a0d023e62d95397265
n <not@provided.nil>
2019-08-03 14:31:26 +0100

    Added login route and jwtokens

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/users');
 
 const connUri = process.env.MONGO_LOCAL_CONN_URL;
@@ -25,6 +27,56 @@ module.exports = {
             } else {
                 status = 500;
                 result.status = err;
+                result.error = err;
+                res.status(status).send(result);
+            }
+        });
+    },
+    login: (req, res) => {
+        const { name, password } = req.body;
+
+        mongoose.connect(connUri, { useNewUrlParser: true }, err => {
+            let result = {};
+            let status = 200;
+            if(!err) {
+                User.findOne({ name }, (err, user) => {
+                    if (!err && user) {
+                        // It'd be possible to compare pass in model instead. We'll see.
+                        bcrypt.compare(password, user.password).then(match => {
+                            if(match) {
+                                status = 200;
+                                // Creates a token
+                                const payload = { user: user.name };
+                                // TODO: Change issuer to final domain
+                                const options = { expiresIn: '2d', issuer: 'test' };
+                                const secret = process.env.JWT_SECRET;
+                                const token = jwt.sign(payload, secret, options);
+
+                                result.token = token;
+                                result.status = status;
+                                result.result = user;
+                            } else {
+                                status = 401;
+                                result.status = status;
+                                result.error = 'Authentication error';
+                            }
+                            res.status(status).send(result);
+                        }).catch(err => {
+                            status = 500;
+                            result.status = status;
+                            result.error = err;
+                            res.status(status).send(result);
+                        });
+                    } else {
+                        status = 404;
+                        result.status = status;
+                        result.error = err;
+                        res.status(status).send(result);
+                    }
+                });
+            } else {
+                status = 500;
+                result.status = status;
                 result.error = err;
                 res.status(status).send(result);
             }
